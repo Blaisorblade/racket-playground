cea32c6159761b05ccf68100684a67480748ab07
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2015-09-30 14:58:07 +0200

    Move away files

--- symbol-utils.rkt
+++
@@ -1,7 +0,0 @@
-#lang racket
-(provide (all-defined-out))
-
-(define ((symbol-format formatStr) symbol) (string->symbol (format formatStr symbol)))
-
-(define (gensym-preserving orig)
-  (gensym ((symbol-format "~a:") orig)))
